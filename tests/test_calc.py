197254f2fb423ea45b80726639cfde812e618157
masinakh <nakhjirimasi@gmail.com>
2022-09-21 12:25:44 -0700

    wave4 is done

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,14 +2,25 @@ import pytest
 
 from average_calculator.calc import rounded_average
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_average_of_two_numbers_is_properly_computed():
-    actual = rounded_average([4, 6])
-    # Delete the skip and write the proper assertion here!
-    pass
+    #Arrange
+    num_list = [4,6] 
+    
 
-@pytest.mark.skip
+    #Act
+    actual = rounded_average(num_list)
+    # Delete the skip and write the proper assertion here!
+    #Assert
+    assert actual == 5
+
 def test_average_of_empty_list_raises_ValueError():
     # Delete the skip and write the test here!
     # Hint: use pytest.raises
-    pass
+
+    with pytest.raises(ValueError):
+        rounded_average([])
+
+    
+
+   
